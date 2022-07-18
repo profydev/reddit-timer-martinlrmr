8e93df8960d081df5a3cf91c88730d2fb2ff4a0f
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-18 16:08:32 +0200

    Import GlobalStyles into App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import GlobalStyles from './globalStyles';
 
 function App() {
   return (
-    <div>
+    <>
+      <GlobalStyles />
       App Placeholder
-    </div>
+    </>
   );
 }
 
