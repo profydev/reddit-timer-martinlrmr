42d725ec67f64f5c9a3e816295729d99afaaac64
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-18 18:26:58 +0200

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import GlobalStyles from './globalStyles';
 
 function App() {
   return (
-    <div>
-      App Placeholder
-    </div>
+    <>
+      <GlobalStyles />
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            (
+              <>
+                <h1>test</h1>
+              </>
+            )
+      } />
+        <Route
+          path="/search"
+          element={
+            (
+              <>
+                <h1>test 2</h1>
+              </>
+            )
+      } />
+      </Routes>
+    </>
   );
 }
 
