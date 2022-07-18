1e0afa8c6a4e467bb0f4b21583ab988912bd5faf
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-18 23:15:34 +0200

    Test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders App', () => {
-  render(<App />);
+  render(
+  <Router>
+    <App />
+  </Router>
+  );
 });
