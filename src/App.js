ec6a803ac954e626a0acf8fc91b909c555a8c8b8
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-18 16:37:22 +0200

    Add "/" and "/search" routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './globalStyles';
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      App Placeholder
+
+      <Link to="/">Home</Link>
+      <Link to="/search">Search</Link>
+
+      <Routes>
+        <Route exact path="/">
+          App Placeholder
+        </Route>
+
+        <Route path="/search">
+          test
+        </Route>
+      </Routes>
+
     </>
   );
 }
