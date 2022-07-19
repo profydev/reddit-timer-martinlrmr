c08b749a37a54a9c9c6d665a62f5fb5cc277b006
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-19 17:32:14 +0200

    Add styled-normalize

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { Normalize } from 'styled-normalize';
 import GlobalStyles from './components/globalStyles';
 
 function App() {
   return (
     <>
-      <GlobalStyles />
       <Routes>
+        <Normalize />
+        <GlobalStyles />
         <Route path="/" element={<h1>Home Page</h1>} />
         <Route path="/search" element={<h1>Search</h1>} />
       </Routes>
