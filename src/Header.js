cb8ec2c8456e238eb15b0b83e19a1a422f852224
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-19 11:46:57 +0200

    Styling

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import * as Styled from './Header.styled';
 import logo from './img/logo.svg';
 
 function Header() {
   return (
-    <header>
-      <ul>
-        <li>
-          <Link to="/">
-            <img src={logo} alt="" />
-          </Link>
-        </li>
-        <li><Link to="/search/javascript">Search</Link></li>
-        <li><Link to="#how-it-works">How It Works</Link></li>
-        <li><Link to="#about">About</Link></li>
-      </ul>
-    </header>
+    <Styled.Header>
+      <Styled.LinkImg to="/">
+        <Styled.Img src={logo} alt="" />
+      </Styled.LinkImg>
+      <Styled.Ul>
+        <Styled.Li><Styled.LinkLi to="/search/javascript">Search</Styled.LinkLi></Styled.Li>
+        <Styled.Li><Styled.LinkLi to="/#how-it-works">How It Works</Styled.LinkLi></Styled.Li>
+        <Styled.Li><Styled.LinkLi to="/#about">About</Styled.LinkLi></Styled.Li>
+      </Styled.Ul>
+    </Styled.Header>
   );
 }
 
---
+++ src/Header.styled.js
@@ -0,0 +1,38 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Header = styled.header`
+  display: flex;
+  padding: 32px 80px 32px 80px;
+`;
+
+export const LinkImg = styled(Link)`
+  display: flex;
+  margin-right: auto;
+`;
+
+export const LinkLi = styled(Link)`
+  font-family: 'Montserrat';
+  font-size: 1rem;
+  color: #636363;
+  text-decoration: none;
+  cursor: pointer;
+`;
+
+export const Ul = styled.ul`
+  display: flex;
+  gap: 1.5rem;
+  align-items: center;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+
+`;
+
+export const Li = styled.li`
+  color: red;
+`;
+
+export const Img = styled.img`
+  width: 150px;
+`;
