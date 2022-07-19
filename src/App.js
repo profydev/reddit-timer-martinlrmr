c3a358c04a1fa1d1363f2299e8144204330b32fe
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-19 17:23:46 +0200

    Move globalStyles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import GlobalStyles from './globalStyles';
+import GlobalStyles from './components/globalStyles';
 
 function App() {
   return (
