586d2099df22ddef05d374526a89dc7ccf87e2a5
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-21 23:18:19 +0200

    Move link and h1 styling to global styles

--- src/style/globalStyles.js
+++ src/style/globalStyles.js
@@ -10,6 +10,20 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     height: 100vh;
   }
+
+  h1 {
+    font-family: ${(props) => props.theme.font.family.headline};
+    font-weight: 400;
+    font-size: 2.375rem;
+    color: ${(props) => props.theme.color.dark};
+  }
+
+  a {
+    font-family: 'Montserrat';
+    color: ${(props) => props.theme.color.midDark};
+    text-decoration: none;
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyle;
