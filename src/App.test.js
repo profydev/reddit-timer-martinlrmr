09ed9ac4840cc0564658fa49abb2151dc5c29501
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-21 11:49:46 +0200

    lint fixes

--- src/App.test.js
+++ src/App.test.js
@@ -5,23 +5,23 @@ import userEvent from '@testing-library/user-event';
 import App from './App';
 
 function setup() {
-  render (
+  render(
     <MemoryRouter>
       <App />
     </MemoryRouter>,
   );
-};
+}
 
 test('renders App', () => {
   setup();
-  screen.debug()
+  screen.debug();
 });
 
 describe('Header', () => {
   test('"Logo" links to correct url', () => {
     setup();
     const link = screen.getByRole('link', { name: /logo/i });
-    userEvent.click(link)
+    userEvent.click(link);
 
     expect(
       screen.getByRole('heading', { name: /Home Page/i })
