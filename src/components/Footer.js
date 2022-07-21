63932622fe13c9edfb5746b9fe5d65b954401530
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-21 13:40:50 +0200

    Add link to profy.dev

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,9 @@ import logoFooter from '../img/logo-footer.svg';
 function Footer() {
   return (
     <S.Footer>
+      <S.A href="https://profy.dev/employers" target="_blank" rel="noreferrer">
+        profy.dev
+      </S.A>
       <S.LinkImg to="/" aria-label="logo-footer">
         <S.Img src={logoFooter} />
       </S.LinkImg>
--- src/components/Footer.style.js
+++ src/components/Footer.style.js
@@ -4,10 +4,13 @@ import styled from 'styled-components';
 export const Footer = styled.footer`
 `;
 
-export const LinkImg = styled(UnstyledLink)`
+export const A = styled.a`
 `;
 
 export const Link = styled(UnstyledLink)`
+`;
+
+export const LinkImg = styled(UnstyledLink)`
 `;
 
 export const Li = styled.li`
