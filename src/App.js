ab0eab09a61c8667526cfac8e1f306dbf97410b0
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-21 23:09:04 +0200

    Install prop-types

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { Normalize } from 'styled-normalize';
@@ -11,17 +11,18 @@ import Homepage from './pages/Homepage';
 import Footer from './components/Footer';
 
 function App() {
-  const [subreddit, setSubreddit] = useState('javascript')
+  const subreddit = 'javascript';
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <Normalize />
         <GlobalStyles />
-        <Header />
+        <Header subreddit={subreddit} />
         <S.ContentContainer>
           <Routes>
             <Route path="/search" element={<h1>Search</h1>} />
-            <Route path="/" element={<Homepage subreddit={subreddit}/>} />
+            <Route path="/" element={<Homepage subreddit={subreddit} />} />
           </Routes>
         </S.ContentContainer>
         <Footer />
