64fb6999f35dda5db5e4e3aad4058bf8476d512d
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-24 20:18:47 +0200

    Added filtering of data and display of amount of posts in cells
    - each combination of weekday and hour of the day is represented by a square box
    - each box contains the corresponding number of the posts at this weekday and hour

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -1,10 +1,30 @@
 import * as React from 'react';
 import * as S from './Heatmap.style';
+import tempdata from './tempdata';
 
 function Heatmap() {
-  const array = (
-    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]);
-  const renderCells = (input) => input.map((redditPost) => <S.DataCell>{redditPost}</S.DataCell>);
+  const weekdays = [...Array(7).keys()];
+  const hours = [...Array(24).keys()];
+
+  const sortedRedditPosts = weekdays.reduce((dayAcc, weekday) => {
+    const redditPostsOfThisDay = tempdata.filter(((redditPost) => redditPost.weekday === weekday));
+    const redditPostsSeperatedByHour = hours.map((hour) => (
+      redditPostsOfThisDay.filter((redditPost) => redditPost.hour === hour)
+    ));
+
+    return { ...dayAcc, [`day${weekday}`]: redditPostsSeperatedByHour };
+  }, {});
+
+  console.log(sortedRedditPosts);
+
+  function renderCells(redditPostsOfThisDay) {
+    return redditPostsOfThisDay.map((hour) => {
+      if (hour.length > 0) {
+        return <S.DataCell>{hour.length}</S.DataCell>;
+      }
+      return <S.DataCell>0</S.DataCell>;
+    });
+  }
 
   return (
     <S.Table>
@@ -27,31 +47,31 @@ function Heatmap() {
       <tbody>
         <tr>
           <S.RowTitles>Sunday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day0)}
         </tr>
         <tr>
           <S.RowTitles>Monday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day1)}
         </tr>
         <tr>
           <S.RowTitles>Tuesday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day2)}
         </tr>
         <tr>
           <S.RowTitles>Wednesday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day3)}
         </tr>
         <tr>
           <S.RowTitles>Thursday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day4)}
         </tr>
         <tr>
           <S.RowTitles>Friday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day5)}
         </tr>
         <tr>
           <S.RowTitles>Saturday</S.RowTitles>
-          {renderCells(array)}
+          {renderCells(sortedRedditPosts.day6)}
         </tr>
       </tbody>
     </S.Table>
