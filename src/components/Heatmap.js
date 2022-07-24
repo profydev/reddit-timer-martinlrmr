8975b8df5e41096d98f7f2b66f7f206474d3bed3
martinlrmr <102468994+martinlrmr@users.noreply.github.com>
2022-07-24 10:59:35 +0200

    Add row showing the hours from left to right (12:00am to 11:00pm)

--- src/components/Heatmap.js
+++ src/components/Heatmap.js
@@ -5,6 +5,22 @@ function Heatmap() {
   return (
     <S.Table>
       <tr>
+        <thead>
+          <S.ColTitles> </S.ColTitles>
+          <S.ColTitles colSpan="2">12.00am</S.ColTitles>
+          <S.ColTitles colSpan="2">2.00am</S.ColTitles>
+          <S.ColTitles colSpan="2">4.00am</S.ColTitles>
+          <S.ColTitles colSpan="2">6.00am</S.ColTitles>
+          <S.ColTitles colSpan="2">8.00am</S.ColTitles>
+          <S.ColTitles colSpan="2">10.00am</S.ColTitles>
+          <S.ColTitles colSpan="2">12.00pm</S.ColTitles>
+          <S.ColTitles colSpan="2">2.00pm</S.ColTitles>
+          <S.ColTitles colSpan="2">4.00pm</S.ColTitles>
+          <S.ColTitles colSpan="2">6.00pm</S.ColTitles>
+          <S.ColTitles colSpan="2">8.00pm</S.ColTitles>
+          <S.ColTitles colSpan="2">10.00pm</S.ColTitles>
+        </thead>
+
         <tbody>
           <tr>
             <S.RowTitles>Monday</S.RowTitles>
